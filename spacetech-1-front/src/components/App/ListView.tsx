070d562a374ba0d0e93bb9b8951349e751de9ee5
letier3110 <letier3110@gmail.com>
2024-08-18 00:15:52 +0400

    feat: added front

---
+++ spacetech-1-front/src/components/App/ListView.tsx
@@ -0,0 +1,49 @@
+import { FC, useState } from 'react'
+import { DataEntry } from '../../lib/interfaces'
+// import tagGroups from '../../../public/filters.json'
+// import PlaceModeData from '../../../public/mode.json'
+// import Card from '../card/Card'
+import Map from '../Map/Map'
+
+// interface IFilter {
+//   search: string
+//   tags: string[]
+//   sort: string
+// }
+
+interface ListViewProps {
+  data: Array<DataEntry>
+}
+
+export const ListView: FC<ListViewProps> = () => {
+  const [mapMode] = useState(true)
+
+  const ViewMode = (
+    <div className='ViewMode'>
+      <button className='ViewModeBtn outline' onClick={() => {}}>
+        {mapMode ? 'Map View' : 'Grid View'}
+      </button>
+    </div>
+  )
+
+  return (
+    <div className='flex flex-column flex-gap AppContainer'>
+      <div className='Header'>
+        {/* <div className='flex flex-gap'>{ViewMode}</div> */}
+      </div>
+      {/* <div className='MobileHeader'>{ViewMode}</div> */}
+
+      {mapMode && (
+        <div className='CardsWithMap'>
+          <div className='MapContent'>
+            {/* {filteredContent.map((cardData, index) => (
+              <Card cardData={cardData} index={index + 1} key={index} />
+            ))} */}
+          </div>
+          <Map data={[]} />
+        </div>
+      )}
+      <div className='bottom'></div>
+    </div>
+  )
+}
--- spacetech-1-front/src/main.tsx
+++ spacetech-1-front/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import 'mapbox-gl/dist/mapbox-gl.css';
+import App from './components/App/App.tsx'
 import './index.css'
+import './font.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
